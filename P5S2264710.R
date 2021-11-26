b3140f2072742716b445b11538ab4446fd53e04c
unknown <aprabaswara@gmail.com>
2021-11-26 02:23:00 +0000

    P5S2264710.R

--- P5S2264710.R
+++ P5S2264710.R
@@ -10,6 +10,7 @@ y <-c(1,2,0,2,2,0,4,4,1,9,14,20,22,27,40,46,66,63,105,103,149,159,204,263,326,35
       325,313,306,268,251,259,252,266,256,215,203,196,166,183,162,180,172,167,138,160,144,153,150,122,128,116,
       133,139,122,124,117,92,83,94,109,111,83,86,83,80,73,69)
 
+##new data starting from February 11, 2020
 y_new <- c(rep(0,20),y)
 
 B <- matrix(0,nrow=length(y_new),ncol=length(y_new))
@@ -28,7 +29,7 @@ for (i in 1:nrow(B)){
 ##input to jags
 setwd('C:/Users/Aditya Prabaswara/Bayesian-inference-on-Covid-19-incidence-in-England')
 mod <- jags.model("model.jags",data=list(y=y_new,N=length(y_new),B=B))
-sam <- jags.samples(mod,c("m","n"),n.iter=10000)
+sam <- jags.samples(mod,c("m","n","tau"),n.iter=10000)
 str(sam)
 sam.coda <- coda.samples(mod,c("tau"),n.iter=10000)
 str(sam.coda)
@@ -42,15 +43,23 @@ apply(sam.coda[[1]],2,quantile,prob=(c(0.025,0.975)))
 ##plot for death and incidence
 credible.int <- apply(sam$n,1,quantile,prob=(c(0.025,0.975)))
 max_vertical <- max(sam$n[1:length(y)],credible.int[1,1:length(y)],credible.int[2,1:length(y)],sam$m[1:length(y_new)])
-plot(sam$n[1:length(y)],xlab='Day of the year',ylab='Number of patient',type='l',xlim=c(0,length(y_new)),ylim=c(0,max_vertical),col='green',xaxt='n')
-lines(credible.int[1,1:length(y)],col='red',lty=2)
-lines(credible.int[2,1:length(y)],col='red',lty=2)
-points(y_new,pch=1,col='grey')
-lines(sam$m[1:length(y_new)],col='blue')
-lockdown_day <- julian(as.Date("2020-3-24"),origin=as.Date("2019-12-31"))
-horiz_axis <- c(1,20,40,60,lockdown_day,length(y),length(y_new))
+lockdown_day <- julian(as.Date("2020-3-24"),origin=as.Date("2019-12-31"))[1]
+horiz_axis <- c(1,lockdown_day,length(y),length(y_new))
+horiz_axis <- sort(unique(horiz_axis))
+vertic_axis <- c(0,max(sam$n[1:length(y)]),max(sam$m[1:length(y_new)]))
+vertic_axis <- sort(unique(vertic_axis))
+vertic_axis <- round(vertic_axis,0)
+date_vector <- seq(as.Date("2020-2-11"), by = "days", length.out = 120)
+julian_day <- julian(date_vector,origin=as.Date("2019-12-31"))
+
+plot(julian_day,y_new,xlab='Day of the year',ylab='Incidence',ylim=c(0,max_vertical),col='grey')
+lines(julian_day[1:length(y)],credible.int[1,1:length(y)],col='red',lty=2)
+lines(julian_day[1:length(y)],credible.int[2,1:length(y)],col='red',lty=2)
+lines(julian_day[1:length(y)],sam$n[1:length(y)],col='green')
+lines(julian_day,sam$m[1:length(y_new)],col='blue')
 abline(v=lockdown_day,lty=2)
-axis(1,at=sort(unique(horiz_axis)))
-legend(x='top',legend=c("New Infections", "Death", "Credible Interval"),col=c("green", "blue","red"), lty=c(1,1,2), cex=0.55,bty='n',lwd=1)
-text(x=lockdown_day+1,y=1500,label='UK Lockdown \n (24 March 2020)',pos=1,cex=0.7,srt=90)
-title('Daily Death and New Infections at England',cex.main=1)
+axis(1,at=horiz_axis)
+axis(2,at=vertic_axis)
+legend(x='topright',legend=c("New Infections", "Expected Death", "95% Credible Interval"),col=c("green", "blue","red"), lty=c(1,1,2), cex=0.55,bty='n',lwd=1)
+text(x=lockdown_day+4,y=1400,label='UK Lockdown',pos=3,cex=0.7,srt=90)
+title('Daily Death and New Infections from COVID-19 at England (2020)',cex.main=0.8)
