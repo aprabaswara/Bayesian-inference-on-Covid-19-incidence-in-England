6bbe9d22d1f835e6f9f36c73c99e8610fef448f2
unknown <aprabaswara@gmail.com>
2021-12-03 04:56:43 +0000

    P5S2264710.R

--- P5S2264710.R
+++ P5S2264710.R
@@ -26,6 +26,7 @@ y <-c(1,2,0,2,2,0,4,4,1,9,14,20,22,27,40,46,66,63,105,103,149,159,204,263,326,35
 
 y_new <- c(rep(0,20),y) ##Daily deaths data from February 11, 2020
 
+##Create matrix B
 B <- matrix(0,nrow=length(y_new),ncol=length(y_new))
 
 for (i in 1:nrow(B)){
@@ -40,7 +41,7 @@ for (i in 1:nrow(B)){
 }
 
 
-##Generate Markov Chain Monte Carlo posterior sample using JAGS
+##Generate posterior random sample from 10000 iterations using JAGS
 mod <- jags.model("model.jags",data=list(y=y_new,N=length(y_new),B=B))
 sam.coda <- coda.samples(mod,c("m","n"),n.iter=10000)
 
@@ -49,60 +50,71 @@ par(mfrow=c(4,2),mar=c(2,2,2,2))
 traceplot(sam.coda[[1]][,c('n[2]','n[50]','n[60]','n[100]','m[2]','m[50]','m[60]','m[120]')])
 acfplot(sam.coda[[1]][,c('n[2]','n[50]','n[60]','n[100]','m[2]','m[50]','m[60]','m[120]')],aspect=1,type='l')
 
-##From the trace plot we can see that the chain for m and n have a good mixing near its peaks compare in 
+##From the trace plot we can see that the chain for m and n have a good mixing near its peaks compare near the
+##beginning and the end. In addition, we also found out that the chain autocorrelation reduces faster near
+##its peaks compare to near the beginning and the end. 
 
 ##Recommended sample for iterations
 recommended_sample <- max(effectiveSize(sam.coda[[1]]))
 recommended_sample
 
-##After we run the code, we find a burn-in period from the first iterations until around the 4000 iterations. 
-##So, we recommend to do a burn-in for 4000 sample in the first iteration for the simulations. Because the sampling 
-##is quite random, we would suggest that the effective sample size should be around 400 until 800 sample.
+##After we run the code with different choice of m and n, we find a burn-in period from the first iterations until around the 4000 iterations. So, we recommend 
+##to do a burn-in for 4000 sample in the first iteration for the simulations. Because the sampling is quite random, we would suggest that the effective sample size 
+##should be around 400 until 800 sample. Furthermore, regarding the recommended number of sampling iteration to run,we found out that when we run for 30000 iterations 
+##we get the effective sample size is not too small and the autocorrelation reduces faster compare to using 10000 iterations.
 
-##Find sample for expected death and new infections
+##Find sample for expected death and new infections from the sample list
+
 death_index <- grep("m", colnames(sam.coda[[1]]))
+
 infection_index <- grep("n", colnames(sam.coda[[1]]))[1:length(y)]
 
-##Generate credible interval for new infections
+##Get credible interval for new infections
+
 credible_interval <- apply(sam.coda[[1]][,infection_index],2,quantile,prob=(c(0.025,0.975)))
+
 upper_bound <- credible_interval[2,]
+
 lower_bound <- credible_interval[1,]
 
 ##Posterior mean for new infections and expected death
 expect_death <- colMeans(sam.coda[[1]][,death_index])
+
 new_infect <- colMeans(sam.coda[[1]][,infection_index])
+
 max_vertical <- max(max(new_infect),max(expect_death),max(upper_bound),max(lower_bound),max(y_new))
 lockdown_day <- julian(as.Date("2020-3-24"),origin=as.Date("2019-12-31"))
 date_vector <- seq(as.Date("2020-2-11"), by = "days", length.out = 120)
 julian_day <- julian(date_vector,origin=as.Date("2019-12-31"))
 infection_day <- julian_day[1:(length(y_new)-20)]
 
-##In the single summary plot we will plot the 95% credible interval for
+##In the single summary plot we will plot the 95% credible interval for new infections, the
 ##Reference: 
 #https://stackoverflow.com/questions/14069629/how-can-i-plot-data-with-confidence-intervals
 #https://statisticsglobe.com/r-polygon-function-plot/
 #https://statisticsglobe.com/rev-r-function
 
-##Plot the single summary plot
+##Plot single summary plot
 par(mfrow=c(1,1),mar=c(5.1,4.1,4.1,2.1))
 
 plot(x=julian_day,y=y_new,xlab='Day of the year',ylab='Number of Individuals',ylim=c(0,max_vertical),col='grey',pch=20)
 
 polygon(x=c(rev(infection_day), infection_day), y=c(rev(lower_bound),upper_bound), col = 'gray91', border = NA) ##Draw credible interval region
 
-lines(x=infection_day,y=lower_bound,col='red',lty=2)
+lines(x=infection_day,y=lower_bound,col='grey',lty=2)
 
-lines(x=infection_day,y=upper_bound,col='red',lty=2)
+lines(x=infection_day,y=upper_bound,col='grey',lty=2)
 
-lines(x=infection_day,y=new_infect,col='green')
+lines(x=infection_day,y=new_infect,col='green') ##Plot posterior mean for new infections
 
-lines(x=julian_day,y=expect_death,col='blue')
+lines(x=julian_day,y=expect_death,col='blue') ##Plot posterior mean for expected deaths
 
-##Highlight the the first day of UK lockdown (24th March 2020)
 
-abline(v=lockdown_day,lty=2) ##Draw horizontal line on the lockdown day
+##Highlight the first day of UK lockdown (24th March 2020)
 
-text(x=lockdown_day+4,y=1100,label='lockdown I',pos=3,cex=0.6,srt=90) ##Label the lockdown day with text
+abline(v=lockdown_day,lty=2) 
+
+text(x=lockdown_day+4,y=1100,label='lockdown I',pos=3,cex=0.6,srt=90) 
 
 ##Add legend and title to the single summary plot
 
@@ -112,5 +124,6 @@ legend(x='topright',legend=c("Mean for New Infections", "Mean for Expected Death
 
 title('Daily Death and New Infections from COVID-19 at England (2020)',cex.main=0.8) ##Add plot title
 
+##Conclusion:
 ##From the single summary plot we can see that the posterior mean for the expected death fits well with the daily
 ##death data from NHS. 
